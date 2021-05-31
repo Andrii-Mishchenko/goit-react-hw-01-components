1ed2accc6f891c496e535574b14b017f34221777
Andrii Mishchenko <a.mishenko2001@gmail.com>
2021-05-31 17:35:33 +0300

    HW completed

--- src/components/statistics/StatisticsItem.js
+++ src/components/statistics/StatisticsItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 
 const StatisticsItem =({label, percentage, id})=> (
-    <li className="item" key={id}>
+    <li className="statistics-item" key={id}>
         <span className="label">{label}</span>
         <span className="percentage">{percentage}%</span>
     </li>
